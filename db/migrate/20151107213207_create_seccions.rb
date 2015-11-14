c88bae18ed3b8eb7d1c31538dde1bc64b748fd40
martinnicolas <ttincho77@gmail.com>
2015-11-14 17:13:11 -0300

    Agrego scaffold de clientes

--- db/migrate/20151107213207_create_seccions.rb
+++
@@ -1,9 +0,0 @@
-class CreateSeccions < ActiveRecord::Migration
-  def change
-    create_table :seccions do |t|
-      t.string :nombre
-
-      t.timestamps null: false
-    end
-  end
-end
--- db/migrate/20151112231119_create_clientes.rb
+++
@@ -1,9 +0,0 @@
-class CreateClientes < ActiveRecord::Migration
-  def change
-    create_table :clientes do |t|
-      t.string :nombre
-
-      t.timestamps null: false
-    end
-  end
-end
