faa731e8fd563464cdec4f0bc778c4d882c23013
martinnicolas <ttincho77@gmail.com>
2015-12-01 17:33:08 -0300

    Trabajo sobre mapbox/leaflet

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require mapbox.js
 //= require jquery
 //= require jquery_ujs
 //= require jquery-ui
